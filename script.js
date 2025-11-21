eda44d1b112645e94e933d61c27accdb484b2874
Yashraaj Sharad Gorde <149304566+yashraj-gorde@users.noreply.github.com>
2025-11-21 09:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", handleSubmit);
+
+async function handleSubmit() {
+  const text = document.getElementById("text").value;
+  const delay = Number(document.getElementById("delay").value);
+  const output = document.getElementById("output");
+
+  // wait for delay before displaying message
+  await wait(delay);
+
+  output.innerText = text;
+}
+
+// function returning a promise that resolves after given delay
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
